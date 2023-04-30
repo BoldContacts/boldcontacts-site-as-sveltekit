7b7f4dbd5d9d90a802fd7b45ed72dbf70f7eded3
Joel Parker Henderson <joel@joelparkerhenderson.com>
2023-04-30 11:31:36 +0100

    Init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>import "../app.postcss";</script>
+
+<div class="max-w-xl m-auto">
+    <header></header>  
+    <main class="m-8">
+        <slot></slot>
+    </main>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,154 @@
+<h1>BoldContacts</h1>
+
+<p><a href="https://apps.apple.com/us/app/boldcontacts/id6443335501">BoldContacts on Apple App Store is now free!</a></p>
+
+<p><a href="https://play.google.com/">BoldContacts on Google Play is coming soon.</a></p>
+
+
+<h2>What is BoldContacts?</h2>
+
+<p>BoldContacts™ is a mobile app to help you browse your contacts and connect with them.</p>
+
+<p>BoldContacts is especially good for people who may be experiencing vision disabilities, or cognitive disabilities, or motor disabilities. The app displays each contact with a large photo (if available), large font, and large buttons.</p>
+ 
+<p>BoldContacts started because our family members have recently been diagnosed with Alzheimer's disease and Parkinson's disease, and are now unable to use the typical contacts app. The app may also help people with Multiple Sclerosis (MS), Amyotrophic Lateral Sclerosis (ALS), and Muscular Distrophy (MD), and more.</p>
+
+<p>BoldContacts helps our family keep in touch, and helps caretakers connect with us. We hope it may help you and your loved ones. We welcome constructive feedback. We are actively seeking donations and grants to work on accessibility software full time. Contact Joel Parker Henderson at <a href="mailto:joel@joelparkerhenderson.com">joel@joelparkerhenderson.com</a></p>
+
+
+<h2>How it works</h2>
+
+<p>The app shows each contact's name, a big photo if available, and big buttons.</p>
+
+<ul>
+
+<li>A phone button means "contact this person now". This button connects you by using your choice of video, or phone, or email, or message, or custom URL. A caregiver can customize this by using the default iOS Contacts app.</li>
+
+<li>A left-arrow button means "show the previous contact".</li>
+    
+<li>A right-arrow button means "show the next contact".</li>
+
+</ul>
+
+<p>The buttons are purposefully far apart, to help people see them better, and to help people be able to tap each button separately.</p>
+
+<p>The app saves brief anonymous usage data in your "Documents" directory in a file named "BoldContacts.txt". The data is for diagnostics and disability assistance.</p>
+
+
+<h2>Setup</h2>
+
+<p>The first time you launch BoldContats, you will see an Apple iOS prompt that asks for you to approve this access.</p>
+
+<p>Say yes.</p>
+
+<h2>Personalization</h2>
+
+<p>You can personalize how your BoldContacts app will connect to your contact:</p>
+
+<ol>
+    
+<li>Launch your typical Apple Contacts app, and edit the contact.</li>
+
+<li>Create a new contact field i.e. phone number, or email address, or URL address.</li>
+
+<li>Edit the new contact field label to be "BoldContacts".</li>
+
+</ol>
+
+<h2>Localization and Internationalization</h2>
+
+<p>The app has localization and internationalization for many worldwide languages, including Afrikaans, Amharic, Arabic, Armenian, Assamese, Azerbaijani, Bangla, Basque, Belarusian, Bosnian, Bulgarian, Catalan, Chinese, Corsican, CroationC, Danish, Dutch, English, Esperanto, Finnish, French, Galician, German, Greek, Gujarati, Hawaiian, Hebrew, Hindi, Hmong, Icelandic, Igbo, Indonesian, Irish, Italian, Japanese, Javanese, Kannada, Khmer, Korean, Lao, Latin, Lingala, Lithuanian, Malay, Maori, Marathi, Meiteilon, Nepali, Norwegian, Persian, Polish, Portugese, Punjabi, Quechua, Russian, Samoan, Sanskit, Scots Gaelic, Shona, Slovenian, Swahili, Swedish, Tamil, Telegu, Thai, Turkish, Turkmen, Ukranian, Urdu, Uyghur, Uzbek, Vietnamese, Welsh, Xhosa, Yiddish, Yoruba, Zulu.</p>
+
+<p>We welcome help with localization and internationalization, improving the phrasings and translations, and adding more languages and examples.</p>
+
+
+<h2>Help Wanted</h2>
+
+<p>Contact the author at <a href="mailto:joel@joelparkerhenderson.com">joel@joelparkerhenderson.com</a>.</p>
+
+<ul>
+
+<li>Are you experiencing disabilities? We welcome your feedback.</li>
+
+<li>Are you caring for someone who is experiencing disabilities? We welcome your feedback.</li>
+
+<li>Are you a patron or fundraiser? We're seeking help with donations, grants, and fundraising.</li>
+
+<li>Are you a developer or programmer? We're seeking software technologist to build using iOS Swift and Android Kotlin?</li>
+
+</ul>
+
+
+<h2>Goals</h2>
+
+<p>Good usability for people who experience motor difficulties.</p>
+    
+<ul>
+    <li>Examples: arthritis, nerve damage, reflex tremor, etc.</li>
+</ul>
+
+<p>Good usability for people who experience vision difficulties.</p>
+
+<ul>
+    <li>Examples: partial vision, double vision, blurred vision, etc.</li>
+</ul>
+
+<p>Good usability for people who experience cognitive difficulties.</p>
+
+<ul>
+    <li>Examples:  memory loss, mental impairment, confusion, etc.</li>
+</ul>
+
+
+<h2>For more information</h2>
+
+<p>Wikipedia links:</p>
+
+<ul>
+
+    <li><a href="https://wikipedia.org/wiki/Alzheimer's_disease">Alzheimer's disease</a></li>
+
+    <li><a href="https://wikipedia.org/wiki/Amyotrophic_lateral_sclerosis">Amyotrophic lateral sclerosis</a></li>
+
+    <li><a href="https://en.wikipedia.org/wiki/Multiple_sclerosis">Multiple sclerosis</a></li>
+
+    <li><a href="https://wikipedia.org/wiki/Muscular_dystrophy">Muscular dystrophy</a></li>
+
+    <li><a href="https://wikipedia.org/wiki/Parkinson's_disease">Parkinson's disease</a></li>
+
+</ul>
+
+<p>Organization links:</p>
+
+<ul>
+
+    <li><a href="https://www.aarp.org">AARP</a></li>
+
+    <li><a href="https://www.als.org">ALS Association</a></li>
+
+    <li><a href="https://www.alz.org">Alzheimer's Association</a></li>
+
+    <li><a href="https://alzfdn.org">Alzheimer’s Foundation of America</a></li>
+
+    <li><a href="https://www.apdaparkinson.org">American Parkinson's Disease Association</a></li>
+
+    <li><a href="https://davisphinneyfoundation.org">Davis Phinney Foundation For Parkinson's</a></li>
+
+    <li><a href="http://www.mdausa.org">Muscular Dystrophy Association</a></li>
+
+    <li><a href="https://www.nationalmssociety.org">National Multiple Sclerosis Society</a></li>
+    
+    <li><a href="https://www.parkinson.org">Parkinson's Foundation</a></li>
+
+    <li><a href="https://www.michaeljfox.org">The Michael J. Fox Foundation - Parkinson's</a></li>
+
+</ul>
+
+
+<h2>Legal</h2>
+
+<p>BoldContacts™ is a trademark of Joel Parker Henderson.</p>
+
+<p>Copyright © 2022-2023 by Joel Parker Henderson.</p>
+
+<p>The purpose of the trademark and copyright is to help protect the app and its users from identity theft, or fraud, or hacking.</p>
