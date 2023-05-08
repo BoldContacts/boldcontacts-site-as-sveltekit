85796d5c102c81840d043c76d6fc8dbe03121182
Joel Parker Henderson <joel@joelparkerhenderson.com>
2023-05-08 14:32:30 +0100

    Add more visa info

--- src/routes/uk-global-talent-visa/+page.svelte
+++ src/routes/uk-global-talent-visa/+page.svelte
@@ -8,9 +8,9 @@
 
 <p>This is happening because my mother-in-law has been diagnosed with Alzheimer's, my dad has been diagnosed with Parkinson's, and my friend has been diagnosed with Multiple Sclerosis. I'm learning about how difficult these diseases are, and how I may be able to help a bit by using my programming skills.</p>
 
-<p>The UK Global Talent Visa offers a way forward that enables to me to live in the UK full time, and volunteer with the UK NHS, which is the largest healthcare group in the western world. If I am able to get the visa, then I can work on BoldContacts and make it all free forever for everyone in the world.</p>
+<p>The UK Global Talent Visa offers a way forward because it approves me to live in the UK and to volunteer with the UK NHS, which is the largest healthcare group in the western world. If the UK grants the visa, then I can work on BoldContacts for the entire next year, and also make it all free forever for everyone in the world.</p>
 
-<p>I'm seeking advice and mentorship for BoldContacts, how to help more people, and the visa process. If you have suggestions, ideas, insights, or potential contacts, I will greatly appreciate connecting with you. My email address is joel@boldcontacts.org.</p>
+<p>I'm seeking advice and mentorship for BoldContacts, how to help more people, and leads that may be able to help the UK grant the visa. If you have suggestions, ideas, insights, or potential contacts, I will greatly appreciate connecting with you. My email address is joel@boldcontacts.org.</p>
 
 <p>Best,<br>Joel</p>
 
