6b069096bbd0031984d2f085ac6babacc423a160
Joel Parker Henderson <joel@joelparkerhenderson.com>
2023-05-08 13:20:15 +0100

    Rephrase visa info

--- src/routes/uk-global-talent-visa/+page.svelte
+++ src/routes/uk-global-talent-visa/+page.svelte
@@ -4,13 +4,13 @@
 
 <h1>UK Global Talent Visa</h1>
 
-<p>I'm Joel Parker Henderson, founder of BoldContacts. I'm applying for a United Kingdom Global Talent Visa. I'm asking many people for advice and help me, because my goal for the visa is to move to the UK and work on BoldContacts full time, all for free, all open source.</p>
+<p>I'm Joel Parker Henderson, founder of BoldContacts. I'm applying for a United Kingdom Global Talent Visa. I'm asking everyone for advice and help, because my goal for the visa is to move to the UK and work on BoldContacts full time, all for free, all open source.</p>
 
 <p>This is happening because my mother-in-law has been diagnosed with Alzheimer's, my dad has been diagnosed with Parkinson's, and my friend has been diagnosed with Multiple Sclerosis. I'm learning about how difficult these diseases are, and how I may be able to help a bit by using my programming skills.</p>
 
 <p>The UK Global Talent Visa offers a way forward that enables to me to live in the UK full time, and volunteer with the UK NHS, which is the largest healthcare group in the western world. If I am able to get the visa, then I can work on BoldContacts and make it all free forever for everyone in the world.</p>
 
-<p>I'm actively seeking advice and mentorship with this visa process. If you have suggestions, ideas, insights, or potential contacts, I will greatly appreciate connecting with you. My email address is joel@boldcontacts.org.</p>
+<p>I'm seeking advice and mentorship for BoldContacts, how to help more people, and the visa process. If you have suggestions, ideas, insights, or potential contacts, I will greatly appreciate connecting with you. My email address is joel@boldcontacts.org.</p>
 
 <p>Best,<br>Joel</p>
 
