c1af4738ef29eb75ee5443fa2ee886969adc7c28
Joel Parker Henderson <joel@joelparkerhenderson.com>
2023-05-15 18:57:21 +0100

    Add quotations

---
+++ src/routes/quotations/+page.svelte
@@ -0,0 +1,33 @@
+<div class="max-w-2xl m-auto">
+
+<img src="/assets/screenshot.png" alt="Screenshot gallery of BoldContacts on an iPhone" loading="lazy">
+
+<h1>Quotations request</h1>
+    
+Purpose:
+
+<ul>
+
+    <li>Expert quotations help inform readers about the importance of elderly care work, and that the sector needs people who are ready, willing, and able to help.</li>
+
+    <li>Expert quotations are for supplementary support in my UK Global Talent Visa application. The quotation is informal and friendly; it is not any kind of formal endorsement, nor official letter of recommendation, nor financial commitment, etc.</li>
+
+</ul>
+
+Examples:
+
+<ul>
+    
+    <li>"I research elderly care [etc.]. I know that the UK elderly population needs [etc.]. I've spoken with Joel about his product BoldContacts. It is an innovative product; as far as I know, there's no equivalent in use."</li>
+
+    <li>"I am an expert in [etc.]. We want to work with smart motivated people in related. Joel can help because he's an expert software developer, and he wants to move to the UK to be of service."</li>
+
+    <li>"[your project here] is working on [your goal here]. Joel is offering a technology that may be able to help with this kind of work, and also with similar kinds of work by related groups."</li>
+
+    <li>"Please consider Joel for the UK Global Talent Visa. I'm working on [your initiatives] and I appreciate his development of free open source software. I believe the UK can leverage his skills."</li>
+
+    <li>"I am an academic expert in [your area]. I've talked with Joel about how his abilties align with this. I hope that UK leadership looks favorably on him as he applies for the UK Global Talent Visa."</li>
+
+</ul>
+
+</div>
