8e4c7512b0703138ee28fffb3c2f85a47b56f93d
Joel Parker Henderson <joel@joelparkerhenderson.com>
2023-05-15 18:57:31 +0100

    Add skill space

---
+++ src/routes/skill-space/+page.svelte
@@ -0,0 +1,55 @@
+<div class="max-w-2xl m-auto">
+
+    <img src="/assets/screenshot.png" alt="Screenshot gallery of BoldContacts on an iPhone" loading="lazy">
+    
+    <h1>Skill space</h1>
+        
+    Purpose: Provide a physical place that is accessible to people who are elderly, people who are disabled, and their carers, where the people can get hands-on help with new skills.
+
+    BoldContacts is one skill of many that can be shared in the skill space. We have volunteers who are interested in helping with many more kinds of skills, for people of all ages and abilities.
+
+    <ul>
+    
+        <li>3D Printing: learn how to use a 3D printer, and the software to design and build 3D physical items.</li>
+
+        <li>Aerospace engineering: learn how to build and fly paper airplanes, using the same ideas as with real aircraft.</li>
+
+        <li>Artwork printmaking: learn how to create a greeting card, including the artwork and the physical cards.</li>
+
+        <li>Herbal treatments: learn how to use herbs and tinctures to improve your well-being and your happiness.</li>
+        
+        <li>Creative writing: learn how to write a poem, short story, or novel; includes practice reading your work.</li>
+
+        <li>Mental health coaching: learn about resources in the community and online that may help you and your loved ones.</li>
+        
+        <li>Phone skills for beginners: learn how to use your iPhone or Android phone for typical mobile applications.</li>
+
+        <li>Social networks for novices: learn how join Facebook and Instagram to connect with your friends and family.</li>
+
+        <li>Therapy at home: learn relaxation techniques that can reduce anxiety, improve mood, and open communication.</li>
+
+    </ul>
+    
+    We are seeking to develop the skill space with this roadmap:
+
+    <ul>
+
+        <li>✅ Gauge initial interest with potential volunteers.</li>
+
+        <li>✅ Create anchor project BoldContats.org for high-need population.</li>
+
+        <li>✅ Locate a potential building that has high-quality access for people who are elderly, or disabled, or using public transportation via bus/train/taxi.</li>
+
+        <li>Discuss the project with the appropriate local council representatives. [IN PROGRES]</li>
+
+        <li>Incorporate as a charity-- this requires UK Global Talent Visa authorization E.t.a. June 2023.</li>
+
+        <li>Secure the building, equipment, volunteer commitments, and social marketing setups. E.t.a. July 2023.</li>
+
+        <li>Offer the space and skill shares to a hand-selected group of testers, to gain feedback. E.t.a. August 2023.</li>
+
+        <li>Launch to the general public. E.t.a. September 2023.</li>
+
+    </ul>
+
+</div>
