f1d802b076b529d90d7b10a1bb010d12ca126a57
Joel Parker Henderson <joel@joelparkerhenderson.com>
2023-05-29 11:12:16 +0100

    Add music page

---
+++ src/routes/music/+page.svelte
@@ -0,0 +1,60 @@
+<h1>BoldContacts music</h1>
+
+<p>BoldContacts can connect to many popular music services, including YouTube, Spotify, Apple Music, and more.</p>
+    
+<img src="/assets/explainers/play/1600x1660.png" width="50%" alt="BoldContacts connects to music services including YouTube, Spotify, Apple Music" loading="lazy">
+
+
+<h2>How to link to music</h2>
+
+<p>Many major music services have ways to link directly to an artist, or album, or song, or video, or playlist.</p>
+
+<p>The link is called a "URL" which stands for "Uniform Resource Locator". The link functions as a web address.</p>
+
+<p>Let's use an example of The Beatles song "I Want To Hold Your Hand".</p>
+
+<p>Each music service has its own way link to music by using a URL. You will need to use whichever way your preferred service uses.</p>
+
+<p>Example URL formats:</p>
+
+<ul>
+
+    <li>
+        <b>YouTube Video:</b>
+        <br>
+        <a href="https://www.youtube.com/watch?v=jenWdylTtzs">https://www.youtube.com/watch?v=jenWdylTtzs</a>
+    </li>
+
+    <li>
+        <b>Spotify Playlist:</b>
+        <br>
+        <a href="https://open.spotify.com/track/4pbG9SUmWIvsROVLF0zF9s">https://open.spotify.com/track/4pbG9SUmWIvsROVLF0zF9s</a>
+    </li>
+
+    <li>
+        <b>Apple Music Song:</b>
+        <br>
+        <a href="https://music.apple.com/us/album/i-want-to-hold-your-hand/1440833098?i=1440833542">https://music.apple.com/us/album/i-want-to-hold-your-hand/1440833098?i=1440833542</a>
+    </li>
+</ul>
+
+
+<h2>How to create a contact</h2>
+
+<p>When you know the URL link to the music you want, then you can create a new contact:</p>
+
+<ol>
+
+    <li>Start your typical iOS Contacts app.</li>
+
+    <li>Tap the "+" icon to create a new contact.</li>
+
+    <li>Type any name you want, such as "The Beatles".</li>
+
+    <li>Upload any photo you want, such as a photo of the Beatles that you download from the internet.</li>
+
+    <li>Add the URL link to your music.</li>
+
+</ol>
+
+Then quit BoldContacts and relaunch it; this tells BoldContacts to refresh your contact list, including your new contact. As an aside, a future release of BoldContacts will add automatic refresh.
