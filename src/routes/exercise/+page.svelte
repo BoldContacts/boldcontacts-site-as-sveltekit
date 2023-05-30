7100156a0ce819a33866ad0985869606e5855432
Joel Parker Henderson <joel@joelparkerhenderson.com>
2023-05-30 21:17:42 +0100

    Add exercise

---
+++ src/routes/exercise/+page.svelte
@@ -0,0 +1,53 @@
+<img src="/assets/pexels/pexels-mikhail-nilov-7500699@1200x600.jpg" alt="People exercising">
+
+<h1>BoldContacts Exercise</h1>
+
+<p>BoldContacts can help people exercise more, by making it easier for people to connect to exercise videos, occupational therapy videos, balance training videos, and medical recovery videos.</p>
+
+<p>Exercise in the elderly is proven to help increase mobility, muscle tone, balance capabilities, and longevity.</p>
+
+<p>Exercise also helps reduce the risk of future injuries, such as from accidental falls, which can cause hip fractures.</p> 
+
+
+<h2>How to link to a exercise video</h2>
+
+<p>Many major video services have ways to link directly to an artist, or album, or song, or video, or playlist.</p>
+
+<p>The link is called a "URL" which stands for "Uniform Resource Locator". The link functions as a web address.</p>
+
+<p>Let's use an example of video for "Exercise".</p>
+
+<p>Each video service has its own way link to video by using a URL. You will need to use whichever way your preferred service uses.</p>
+
+<p>Example URL format:</p>
+
+<ul>
+
+    <li>
+        <b>YouTube Video: 10 Minute Beginner Chair Exercises for Seniors</b>
+        <br>
+        <a href="https://www.youtube.com/watch?v=oumzMyqK-2I">https://www.youtube.com/watch?v=oumzMyqK-2I</a>
+    </li>
+
+</ul>
+
+
+<h2>How to create a contact</h2>
+
+<p>When you know the URL link to the video you want, then you can create a new contact:</p>
+
+<ol>
+
+    <li>Start your typical iOS Contacts app.</li>
+
+    <li>Tap the "+" icon to create a new contact.</li>
+
+    <li>Type any name you want, such as "Wizard of Oz".</li>
+
+    <li>Upload any photo you want, such as a photo of the video that you download from the internet.</li>
+
+    <li>Add the URL link to your video.</li>
+
+</ol>
+
+Then quit BoldContacts and relaunch it. This tells BoldContacts to refresh your contact list, including your new contact. As an aside, a future release of BoldContacts will add automatic refresh.
