639dd706c78e1eaad719ff46183449c7e0899d21
Joel Parker Henderson <joel@joelparkerhenderson.com>
2023-05-31 19:00:09 +0100

    Reformat quotations

--- src/routes/quotations-by-experts/+page.svelte
+++ src/routes/quotations-by-experts/+page.svelte
@@ -42,39 +42,56 @@ Thank you so much for helping me and this project. If you want to know more abou
 <h1>Quotations by experts thus far</h1>
     
 
+
 <h2>Health Innovation Leader</h2>
 
-<blockquote><p>"Care City is a health innovation service based in North East London, and trialling new health innovations across the UK. We were are part of the NHS Testbed. One of our recent projects is working on a dementia wayfinding app to help those with dementia to be able to navigate independently using an app and AR (Augmented Reality).</p><p>We would really like to work with someone as experienced as Joel to get his insights on ensuring the user interface is as simple as possible for this vulnerable customer group. He has already developed an app to support dementia users make video calls in a very simple and clear dementia-friendly way. We would also really like to work with him to develop this product so it can be used freely by social care providers to allow more isolated people in care homes to talk to their relatives who may be around the world."</p></blockquote>
+<blockquote>
+    <p>&ldquo;Care City is a health innovation service based in North East London, and trialling new health innovations across the UK. We were are part of the NHS Testbed. One of our recent projects is working on a dementia wayfinding app to help those with dementia to be able to navigate independently using an app and AR (Augmented Reality).</p>
+    <p>&ldquo;We would really like to work with someone as experienced as Joel to get his insights on ensuring the user interface is as simple as possible for this vulnerable customer group. He has already developed an app to support dementia users make video calls in a very simple and clear dementia-friendly way. We would also really like to work with him to develop this product so it can be used freely by social care providers to allow more isolated people in care homes to talk to their relatives who may be around the world.&rdquo;</p>
+</blockquote>
 - <cite>Ben Williams, Senior Project Lead, Care City Innovation C.I.C.</cite>
 
 
 <h2>University Researcher</h2>
 
-<blockquote><p>"My research over the past 25 years involves examining how older people stay connected to the world around them. In our fast evolving technocentric society, there is significant and pressing need to make sure older people can fully engage with the world around them using technology, enhancing their connectivity with other people and the things they need to do.</p><p>Talking with Joel about his product BoldContacts fits this need perfectly, making technology simple for older people to use so they can stay connected and get their needs met. There is nothing else out there that meets this need in such a simple and effective way."</p></blockquote>
+<blockquote>
+    <p>&ldquo;My research over the past 25 years involves examining how older people stay connected to the world around them. In our fast evolving technocentric society, there is significant and pressing need to make sure older people can fully engage with the world around them using technology, enhancing their connectivity with other people and the things they need to do.</p>
+    <p>&ldquo;Talking with Joel about his product BoldContacts fits this need perfectly, making technology simple for older people to use so they can stay connected and get their needs met. There is nothing else out there that meets this need in such a simple and effective way.&rdquo;</p></blockquote>
 - <cite>Professor Charles Musselwhite, Professor of Psychology, Co-Director of the Centre for Ageing and Dementia Research, Aberystwyth University</cite>
 
 
 <h2>Hospital Sister</h2>
 
-<blockquote><p>"I'm a Sister in NHS A&E. I work with critically ill patients and deal with emergencies every day. My hospital has elderly patients who need to stay for longer durations, even weeks, in Clinical Decisions Unit (CDU) and other wards.</p><p>I know Joel. I have talked with him about his BoldContacts project and the innovation of helping these patients by giving them better access to video calling. I hope you admit him to the UK."</p></blockquote>
+<blockquote>
+    <p>&ldquo;I'm a Sister in NHS A&E. I work with critically ill patients and deal with emergencies every day. My hospital has elderly patients who need to stay for longer durations, even weeks, in Clinical Decisions Unit (CDU) and other wards.</p>
+    <p>&ldquo;I know Joel. I have talked with him about his BoldContacts project and the innovation of helping these patients by giving them better access to video calling. I hope you admit him to the UK.&rdquo;</p>
+</blockquote>
 - <cite>Laura Hackett, Hywel Dda Health Board, Bronglais Hospital</cite>
 
 
 <h2>Mental Health Advocate</h2>
 
-<blockquote><p>"I’m a mental health advocate in Wales and I work to raise awareness of the importance of mental health across the UK. I’ve been interviewed by the BBC a number of times on various mental health topics. I have also taken part in a number of discussions and given speeches at national events, contributed to podcasts and written articles based on my experiences.</p><p>I believe Joel and BoldContacts will be beneficial to our country."</p></blockquote>
+<blockquote>
+    <p>&ldquo;I’m a mental health advocate in Wales and I work to raise awareness of the importance of mental health across the UK. I’ve been interviewed by the BBC a number of times on various mental health topics. I have also taken part in a number of discussions and given speeches at national events, contributed to podcasts and written articles based on my experiences.</p>
+    <p>I believe Joel and BoldContacts will be beneficial to our country.&rdquo;</p>
+</blockquote>
 - <cite>Hedydd Elias, Mental Health Speaker, National Eisteddfod of Wales</cite>
 
 
 <h2>Creative Producer</h2>
 
-<blockquote><p>"I'm a London-based Consultant Creative Producer. Through my capacity as a community events organiser, I understand the importance for the UK's elderly population to stay connected to friends and family to reduce the instance of isolation, promoting better mental health and overall quality of life, and avoiding burdening the NHS and some caregivers who – in the event of elderly loved ones' mental and/or physical decline – may have to sacrifice paid work due to the need to become carers, putting the economy under yet more strain. I've spoken in depth with Joel about BoldContacts, and strongly believe it's a very worthwhile project that could be scaled and rolled out in other areas of Wales and wider UK. My extended network (which comprises a huge number of London-based creatives) includes creatives who collaborate on AI projects with Imperial College London – I'm excited to introduce Joel in particular to this community of creatives and scientists because he's an expert software developer, and he wants to move to the UK to be of service."</p></blockquote>
+<blockquote>
+    <p>&ldquo;I'm a London-based Consultant Creative Producer. Through my capacity as a community events organiser, I understand the importance for the UK's elderly population to stay connected to friends and family to reduce the instance of isolation, promoting better mental health and overall quality of life, and avoiding burdening the NHS and some caregivers who – in the event of elderly loved ones' mental and/or physical decline – may have to sacrifice paid work due to the need to become carers, putting the economy under yet more strain.</p>
+    <p>&ldquo;I've spoken in depth with Joel about BoldContacts, and strongly believe it's a very worthwhile project that could be scaled and rolled out in other areas of Wales and wider UK. My extended network (which comprises a huge number of London-based creatives) includes creatives who collaborate on AI projects with Imperial College London – I'm excited to introduce Joel in particular to this community of creatives and scientists because he's an expert software developer, and he wants to move to the UK to be of service.&rdquo;</p>
+</blockquote>
 - <cite>Fi Dunphy, Founder, North London Maker's Market</cite>
 
 
 <h2>Well-Being Director</h2>
     
-<blockquote><p>"I’m a creative free dance teacher and director of a local well-being hub. I am currently working with elders some of whom are living with dementia in the community and in local nursing and care homes. In addition to the potential benefits of Joel’s Bold Contact’s app to help remove barriers to social isolation, I am currently working on a project to support those living with dementia, their carers and families to create personal playlists of music as a supportive intervention. One of the main challenges we face is having easy to use technology to store and access this music. Joel is offering technology and training that may be able to help with this kind of work, and with similar kinds of work by related groups."</p></blockquote>
+<blockquote>
+    <p>&ldquo;I’m a creative free dance teacher and director of a local well-being hub. I am currently working with elders some of whom are living with dementia in the community and in local nursing and care homes. In addition to the potential benefits of Joel’s Bold Contact’s app to help remove barriers to social isolation, I am currently working on a project to support those living with dementia, their carers and families to create personal playlists of music as a supportive intervention.</p>
+    <p>&ldquo;One of the main challenges we face is having easy to use technology to store and access this music. Joel is offering technology and training that may be able to help with this kind of work, and with similar kinds of work by related groups.&rdquo;</p>
+</blockquote>
 - <cite>Alexander Holloway, The Joyful Life Guide</cite>
-
 
