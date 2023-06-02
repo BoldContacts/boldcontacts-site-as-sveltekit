40b5ca43dc1660845f62312ebe664792e76fb252
Joel Parker Henderson <joel@joelparkerhenderson.com>
2023-06-02 18:31:10 +0100

    Add storytime

---
+++ src/routes/storytime/+page.svelte
@@ -0,0 +1,19 @@
+<p><img src="/thebookshopbythesea/explainer.png" alt="BoldContacts & The Bookshop By The Sea" loading="lazy"></p>
+
+<h1>Storytime Pilot</h1>
+
+<p>Storytime is a pilot project proposal by Freya Blyth, Founder, The Bookshop By The Sea:</p>
+
+<p>I am a librarian with over a decade of experience serving the Welsh community. Recently, I embarked on a new journey as the proud owner of The Bookshop By The Sea, a quaint bookstore that has quickly become a literary hub within our beloved town.</p>
+
+<p>I am reaching out to you today with an exciting idea that I believe could have a profound impact on our community. As a librarian, I have witnessed firsthand the transformative power of stories, and now, as a bookshop owner, I am committed to creating an environment where stories come alive, fostering connections among people of all ages.</p>
+
+<p>Our efforts at The Bookshop By The Sea have been recognized with the esteemed Retail of the Year Award and the Sustainability Award for Community Engagement. These accolades validate our commitment to strengthening our community through literature and sustainability initiatives.</p>
+
+<p>One of our core beliefs is that stories have the ability to bridge gaps and unite individuals across generations. In our Welsh community, we have noticed the need to strengthen connections between elderly people and children, recognizing the immense value this intergenerational exchange brings. We are eager to launch a new pilot program that will harness the power of stories to connect these two groups in a truly meaningful way.</p>
+
+<p>Our vision is to create a series of captivating video-recorded storytime sessions, featuring enthusiastic storytellers engaging both elderly individuals and children. We want to provide an easy-to-use app that showcases these recorded sessions, specifically designed to address the needs of individuals living with dementia. By combining technology and storytelling, we aim to create a platform that fosters connection, encourages reminiscence, and spreads joy within our community.</p>
+
+<p>Joel, given your exceptional expertise in app development and your dedication to creating impactful community projects, we believe you are the perfect partner for this venture. Your passion for connecting people aligns perfectly with our mission, and your skills in app development make you the ideal candidate to help us bring this vision to life.</p>
+
+<p>We envision the app as a gateway to our recorded storytime sessions, providing a user-friendly interface that caters to the unique requirements of elderly individuals and their families. By collaborating with you, we can create an app that seamlessly integrates technology with the warmth of storytelling, enriching the lives of those affected by dementia and creating lasting memories for generations to come.</p>
