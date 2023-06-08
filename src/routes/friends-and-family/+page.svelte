dd712e3a399a4633fe04b692bc977d887e491c53
Joel Parker Henderson <joel@joelparkerhenderson.com>
2023-06-08 10:21:36 +0100

    Add friends & family

---
+++ src/routes/friends-and-family/+page.svelte
@@ -0,0 +1,11 @@
+<img src="/assets/pexels/pexels-kampus-production-7551617/@1200x600.jpg" alt="Happy family uses a tablet computer">
+
+<h1>For Friends &amp; Family</h1>
+
+<p>When a person has impairments that make it hard to use a phone or tablet, then it can be hard to stay in touch with friends and family. The BoldContacts app can help because it's an easier way to use the phone or tablet to browse contacts and connect to them.</p>
+
+<p>Friends and family can be especially supportive of a loved-one who is experiencing impairments. For example, a friend or family member can help the loved-one set up BoldContacts on their phone or tablet, add the best contacts, add friendly photos, and try a few calls together.</p>
+
+<p>What we see in practice is when a person has more calls and videos with friends and family members, then the person feels more loved, more supported, and more spirited.</p>
+
+<p>What we also see in practice is that ongoing contact with friends and family members can also help with care areas. For example, friends and family can help check that the person has everything they need for their care, and is taking their daily medications, and doing their daily health and wellness exercise, and the like.</p>
