faefd0cbf6e59e1b5564e9ab1e061f9742ceb2df
Joel Parker Henderson <joel@joelparkerhenderson.com>
2023-06-08 13:04:56 +0100

    Add carers & caregivers

---
+++ src/routes/carers-and-caregivers/+page.svelte
@@ -0,0 +1,29 @@
+<img src="/assets/pexels/pexels-marcus-aurelius-6787960/@1200x600.jpg" alt="Two people using an iPad together" loading="lazy">
+
+<h1>Carers &amp; Caregivers</h1>
+
+<p>The BoldContacts app can be especially good for carers and caregivers.</p>
+
+<p>The BoldContacts app enables your caree to make phone calls and video calls more easily. You can help your caree with installing the app, and setting up the contacts the way you want and your caree wants.</p>
+
+<ul>
+
+    <li>For example, you can add yourself as a contact, and add your own photo.</li>
+
+    <li>For example, you can manage your career's contacts, to update them with new information, or delete expired information.</li>
+
+</ul>
+
+<p>The app is typically easy to set up on your caree's device.</p>
+
+<ul>
+
+    <li>To get BoldContacts, download it from the Apple App Store <a href="https://apps.apple.com/us/app/boldcontacts/id6443335501">here</a>.</li>
+
+    <li>BoldContacts runs on any current iPhone and iPad, and also runs on many older iPhones and iPads.</li>
+
+</ul>
+
+<p>I provide pro-bono carer training, if you're interested in learning more about BoldContacts and how it may be helpful for you and your caree.</p>
+
+<p>Questions? Suggestions? Email <a href="joel@boldcontacts.org">joel@boldcontacts.org</a>.</p>
