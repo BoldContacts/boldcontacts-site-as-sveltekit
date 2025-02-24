5605af8fadc08bf53531cbbc83a37e306d02963f
Joel Parker Henderson <joel@joelparkerhenderson.com>
2025-02-24 23:07:00 +0000

    Add how to components

---
+++ src/lib/HowToCreate.svelte
@@ -0,0 +1,17 @@
+<h2>How to create a contact</h2>
+
+<p>When you know the URL, then you can create a new contact:</p>
+
+<ol>
+
+    <li>Start your typical iOS Contacts app.</li>
+
+    <li>Tap the "+" icon to create a new contact.</li>
+
+    <li>Type any name you want, such as "TODO" or "TODO".</li>
+
+    <li>Add any image you want, such as a photo of TODO or TODO.</li>
+
+    <li>Add the URL link to your sports service.</li>
+
+</ol>
